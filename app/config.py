5fc002efd9d5aa8472899adf849e0a14b921bd75
AlissonGRN <alissonnascimento.dev@gmail.com>
2024-10-08 23:41:36 -0300

    update config.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     # URL de conexão ao banco de dados PostgreSQL
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://alisson:alisson123@localhost/minha_api_db')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://alisson:alisson123@localhost/minha_api_db'
     
     # Desabilitamos o track de modificações para evitar overhead
     SQLALCHEMY_TRACK_MODIFICATIONS = False
