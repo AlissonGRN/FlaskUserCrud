f6a87848f43aa514ee3b331ec5ff4ee7d7d2c997
AlissonGRN <alissonnascimento.dev@gmail.com>
2024-10-08 22:02:33 -0300

    create user model

---
+++ app/models/user_model.py
@@ -0,0 +1,21 @@
+from app import db
+from datetime import datetime
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True) # Chave primária
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)  # Email (único)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)  # Data de criação
+    
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'created_at': self.created_at.strftime("%Y-%m-%d %H:%M:%S")
+        }
+
